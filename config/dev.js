bca7941e575410bc31890fd4ada2430a9136c94d
Carl Brunson <carl.brunson@gmail.com>
2020-10-14 19:40:40 -0500

    master

---
+++ config/dev.js
@@ -0,0 +1,13 @@
+module.exports = {
+    googleClientID: '507337540619-4v6d2hth0370tar3nnon7jo78hf5g1v0.apps.googleusercontent.com',
+    googleClientSecret: 'ZCF89tX2NGjEsWUn7QiUSRBC',
+    mongoURI: 'mongodb+srv://p00gz:18kEq9LQ5IUW9ume@wolfpack.txkk5.mongodb.net/p00gz?retryWrites=true&w=majority',
+    cookieKey: 'jlkjfdklajfdajiejijijfdf',
+    stripePublishableKey: 'pk_test_gBMXNqJbuGNz0gKwwIprfPiI00mmHziJcx',
+    stripeSecretKey: 'sk_test_51EYCWgEXexisnC17h7Cf90Rk9uotG1ZJG0G92FR0Kirzpz9O7i1pnga9uMMpLE0OZUReukrWa2ICUbe8kuYTE9bA00FwuKTXMe',
+    sendGridKey: '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',
+    redirectDomain: 'https://localhost:5000',
+    redirectGoogleURI: '/auth/google/callback'
+}
+
+//npx ngrok http 5000
